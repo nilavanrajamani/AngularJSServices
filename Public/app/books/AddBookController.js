2446b66033b1416cb8d77127b1e761266795dadd
Nilavan Rajamani <nilavanrajamani@gmail.com>
2017-03-31 10:21:14 +0000

    Commit

---
+++ Public/app/books/AddBookController.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    angular.module('app')
+           .controller('AddBookController', AddBookController);
+
+    function AddBookController() {
+
+    }
+})();
---
+++ Public/app/books/EditBookController.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    angular.module('app')
+           .controller('EditBookController', ['$routeParams', 'books', '$cookies', '$cookieStore', EditBookController]);
+
+    function EditBookController($routeParams, books, $cookie, $cookieStore) {
+        var vm = this;
+
+        //dataService.getAllBooks()
+        //           .then(function (books) {
+        vm.currentBook = books.filter(function (item) {
+            return item.book_id == $routeParams.bookID;
+        })[0];
+        //    console.log($routeParams.bookID);
+        //});
+
+        vm.setAsFavorite = function () {
+            $cookies.favoriteBook = vm.currentBook.title;
+        }
+
+        $cookieStore.put('lastEdited', vm.currentBook);
+    }
+})();
